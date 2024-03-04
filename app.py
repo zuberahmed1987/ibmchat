6fb4a6ad7a018c78ff83f8eab639ceb7a42dbe1f
zuberahmed1987 <zuberahmed1987@gmail.com>
2024-03-04 14:39:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from ibm_watson_machine_learning.foundation_models import Model
+from langchain import PromptTemplate
+from langchain.chains import LLMChain
+from ibm_watsonx_ai.foundation_models import Model
 from dotenv import load_dotenv
 import streamlit as st
 
 load_dotenv()
+
+st.title('🔗 IBM Chat App')
+
